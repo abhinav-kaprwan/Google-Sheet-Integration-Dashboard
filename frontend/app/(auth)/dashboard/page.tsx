26178dd5d994e812e2fe059627da8fb2809f8374
abhinav-kaprwan <abhinavkaprwan2003@gmail.com>
2025-03-08 22:36:14 +0530

    Deployment error2

--- frontend/app/(auth)/dashboard/page.tsx
+++ frontend/app/(auth)/dashboard/page.tsx
@@ -1,18 +1,9 @@
 "use client";
 
-import { Suspense, useState } from "react";
+import { Suspense} from "react";
 import DashboardContent from "@/components/DashboardContent";
 
 export default function Dashboard() {
-  const [isLoading, setIsLoading] = useState(false);
-
-  if (isLoading) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
-      </div>
-    );
-  }
 
   return (
     <Suspense fallback={<div>Loading Dashboard...</div>}>
